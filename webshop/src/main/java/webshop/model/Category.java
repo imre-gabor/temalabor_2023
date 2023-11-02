8c9cc52ed0b9dea05761cc78b9dee96a8273e2b4
imre-gabor <gabor@aut.bme.hu>
2023-11-02 15:02:05 +0100

    transform model classes to JPA entities

--- webshop/src/main/java/webshop/model/Category.java
+++ webshop/src/main/java/webshop/model/Category.java
@@ -3,16 +3,32 @@ package webshop.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(of = "id")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Category {
 
+	@Id
+	@GeneratedValue
 	private int id;
 	private String name; 
 	
+	@OneToMany(mappedBy = "category")
 	private List<Product> products;	
 	
 	public void addProduct(Product p) {
--- webshop/src/main/java/webshop/model/Product.java
+++ webshop/src/main/java/webshop/model/Product.java
@@ -1,16 +1,32 @@
 package webshop.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(of = "id")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Product {
 
+	@Id
+	@GeneratedValue
 	private int id;
 	private String name;
 	private double price;
 	
-	
+	@ManyToOne
 	private Category category;
+	
 }
