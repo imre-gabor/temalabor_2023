fd7c64ff38ecbde6b7398024af36773856c95184
imre-gabor <gabor@aut.bme.hu>
2023-11-02 15:02:44 +0100

    turn service class to Spring bean, injnect repositories, add transaction

--- webshop/src/main/java/webshop/service/CategoryService.java
+++ webshop/src/main/java/webshop/service/CategoryService.java
@@ -2,14 +2,21 @@ package webshop.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
 import webshop.model.Category;
 import webshop.model.Product;
 import webshop.repository.CategoryRepository;
 
+@RequiredArgsConstructor
+@Service
 public class CategoryService {
 
-	private CategoryRepository categoryRepository;
+	private final CategoryRepository categoryRepository;
 	
+	@Transactional
 	public void discountProductInCategory(String catName, int percent) {
 		List<Category> categories = categoryRepository.findByName(catName);
 		categories.forEach(c -> {
